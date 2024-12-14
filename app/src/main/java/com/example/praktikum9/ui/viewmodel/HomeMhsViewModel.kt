29294c1f523287a5924f6fe5b43716114c7101a7
cahyadie <cahyadiilham12@gmail.com>
2024-12-14 16:31:42 +0700

    nambah data classnya dulu

--- app/src/main/java/com/example/praktikum9/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum9/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,2 +1,44 @@
 package com.example.praktikum9.ui.viewmodel
 import com.example.praktikum9.data.entity.Mahasiswa
+
+
+
+
+class HomeMhsViewModel()
+
+
+
+data class HomeUiState(
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessages: String = ""
+)
+
+
+class DetailMhsViewModel()
+
+
+data class DetailUiState(
+    val detailUiEvent : MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessages: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty : Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+fun Mahasiswa.toDtailUiEvent() : MahasiswaEvent {
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
