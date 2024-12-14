8bc06944da8a296ed60fa763dbed2f72214cda01
cahyadie <cahyadiilham12@gmail.com>
2024-12-14 17:25:17 +0700

    data class DetailMhsViewModel.kt

---
+++ app/src/main/java/com/example/praktikum9/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.praktikum9.ui.viewmodel
+
+import com.example.praktikum9.data.entity.Mahasiswa
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+fun Mahasiswa.toDetailUiEvent():MahasiswaEvent {
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
