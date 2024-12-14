e4a0aec940a131d8cb9084bde2b9952ca729b282
cahyadie <cahyadiilham12@gmail.com>
2024-12-14 16:41:50 +0700

    menanmbahkan query get, delete, dan update di DAO

--- app/src/main/java/com/example/praktikum9/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/praktikum9/data/dao/MahasiswaDao.kt
@@ -1,8 +1,10 @@
 package com.example.praktikum9.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.praktikum9.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
@@ -10,8 +12,18 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface MahasiswaDao {
     //fungsi get all data
-    @Query("select * From mahasiswa ")
+    @Query("select * From mahasiswa ORDER BY nama ASC")
     fun getAllMahasiswa() : Flow<List<Mahasiswa>>
+
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
 }
