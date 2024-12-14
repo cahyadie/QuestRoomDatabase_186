3c6999a5fe8efbb3ff90a5168da66d2704981343
cahyadie <cahyadiilham12@gmail.com>
2024-12-14 16:46:51 +0700

    menanmbahkan query get, delete, dan update di repository, yg di bawah salah commit

--- app/src/main/java/com/example/praktikum9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/praktikum9/repository/RepositoryMhs.kt
@@ -6,4 +6,11 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
     fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
+
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
 }
